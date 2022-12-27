c0b0b27925b7d08fe05775ed1b1d4ab19845bea2
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2022-12-27 23:44:30 +0530

    tag test case

---
+++ src/__tests__/app-tag.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { screen, render, fireEvent } from '@testing-library/react';
+import AppTag from '../components/AppTag/AppTag';
+
+test("testing if the button is clicked", () => {
+  const tagClickTester = jest.fn();
+
+  const { getByTestId } = render(
+    <AppTag onClick={tagClickTester} data-testid="tag" />
+  );
+
+  fireEvent.click(getByTestId('tag'));
+
+  expect(tagClickTester).toHaveBeenCalled();
+});
+
+test("testing if tag console log label on click", () => {
+  console.log = jest.fn();
+
+  function testClick(label) {
+    console.log(label);
+  }
+
+  const { getByTestId } = render(
+    <AppTag label="hello" data-testid="tag" onClick={testClick} />
+  );
+
+  fireEvent.click(getByTestId('tag'));
+
+  expect(console.log).toHaveBeenCalledWith("hello");
+});
+
--- src/components/AppTag/AppTag.jsx
+++ src/components/AppTag/AppTag.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const AppTag = (props) => {
-  const { label = "Tag", onClick } = props
+  const { label = "Tag", onClick, ...rest } = props
   return (
-    <div className="app-tag" onClick={() => onClick(label)}>
+    <div className="app-tag" onClick={() => onClick(label)} {...rest}>
       <div className="app-tag--content">
         <span className="app-tag--title">{label}</span>
         <i className="fas fa-times app-tag--close" />
