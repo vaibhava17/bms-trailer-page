2b6a5056c55e27056a953edff3c212b74cf31daf
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2022-12-28 09:07:36 +0530

    nav bar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-
-// TODO: styling
+import logo from '../../assets/images/bms-logo.svg'
 
 const Navbar = (props) => {
   const { onSearch, ...rest } = props
@@ -8,27 +7,20 @@ const Navbar = (props) => {
   return (
     <nav className="navbar">
       <div className="container">
-        <div className="navbar__logo col-3">
-          <a href="/">Logo</a>
+        <div className="navbar__logo">
+          <a href="/">
+            <img src={logo} alt="logo" loading='lazy' />
+          </a>
         </div>
-        <div className="navbar__search col-5">
+        <div className="navbar__search">
+          <i className='fas fa-search search__icon'></i>
           <input
             type="text"
-            placeholder="Search"
+            placeholder="Search for a Movie, Show and More"
             className='search__input'
             onChange={(e) => onSearch(e.target.value)}
             {...rest}
           />
-        </div>
-        <div className="navbar__menu col-4">
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/">About</a>
-            </li>
-          </ul>
         </div>
       </div>
     </nav>
